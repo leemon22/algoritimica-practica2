6561be7ac9d1f4d31e10864b5a039e2f01ae8730
Pedro Perez <pedrojavierperez2003@gmail.com>
2023-04-10 15:04:34 +0200

    divide y venceras ya funciona perfectamente

--- Dyv/dyv.cpp
+++ Dyv/dyv.cpp
@@ -1,9 +1,12 @@
+#include <cmath>
 #include <cstddef>
 #include <float.h>
 #include <vector>
 #include <algorithm>
 #include "dyv.h"
 #include "referencia.h"
+
+#include <iostream>
 
 using namespace std;
 
@@ -47,11 +50,11 @@ bool menorEjeY(referencia i, referencia j){
 }
 
 bool igualesEjeX(referencia i, referencia j){
-	return i.getVaca().getX() == i.getVaca().getX();
+	return i.getVaca().getX() == j.getVaca().getX();
 }
 
 bool igualesEjeY(referencia i, referencia j){
-	return i.getVaca().getY() == i.getVaca().getY();
+	return i.getVaca().getY() == j.getVaca().getY();
 }
 
 bool iguales(referencia i, referencia j){
@@ -66,8 +69,8 @@ par_vacas algoritmoDyV(const vector<vaca> &vacas, const int UMBRAL){
 	vector<referencia> vx, vy;
 	vx.reserve(vacas.size());
 	vy.reserve(vacas.size());
-	for(auto vaca : vacas){
-		referencia r(vaca);
+	for(int i = 0; i < vacas.size(); i++){
+		referencia r(vacas[i]);
 		vx.push_back(r);
 		vy.push_back(r);
 	}
@@ -75,7 +78,7 @@ par_vacas algoritmoDyV(const vector<vaca> &vacas, const int UMBRAL){
 	// Ordeno los vectores de referencias
 	sort(vx.begin(), vx.end(), menorEjeX);
 	sort(vy.begin(), vy.end(), menorEjeY);
-
+	
 	// Comprobamos que no hay dos vacas iguales
 	for(int i = 0; i < vx.size() - 1; i++){
 		if(iguales(vx[i], vx[i+1])){
@@ -83,7 +86,7 @@ par_vacas algoritmoDyV(const vector<vaca> &vacas, const int UMBRAL){
 			return {v,v};
 		}
 	}
-
+	
 	// Los paso a la función que hace la mayoría del trabajo
 	par_referencias par = algoritmoDyVOrdenado(vx, vy, UMBRAL);
 	return {par.first.getVaca(), par.second.getVaca()};
@@ -91,6 +94,7 @@ par_vacas algoritmoDyV(const vector<vaca> &vacas, const int UMBRAL){
 
 par_referencias algoritmoDyVOrdenado(const vector<referencia> &vx, const vector<referencia> &vy, const int UMBRAL){
 
+	
 	// Si el vector es pequeño devuelvo el resultado usando el algoritmo específico
 	if(vx.size() <= UMBRAL)
 		return algoritmoEspecifico(vx);
@@ -110,7 +114,7 @@ par_referencias algoritmoDyVOrdenado(const vector<referencia> &vx, const vector<
 		if(menorEjeX(vx[i], ref_mitad)){
 			vx_superior.push_back(vx[i]);
 		}
-		else {
+		else{
 			vx_inferior.push_back(vx[i]);
 		}
 
@@ -141,7 +145,7 @@ par_referencias algoritmoDyVOrdenado(const vector<referencia> &vx, const vector<
 	}
 
 	// "Coso" las dos mitades
-	float distancia_corregida = distancia*distancia;
+	float distancia_corregida = sqrt(distancia);
 	float coordenada_corte = vx[mitad].getVaca().getX();
 
 	vector<referencia> cercanos_al_corte;
@@ -169,7 +173,7 @@ par_referencias algoritmoDyVOrdenado(const vector<referencia> &vx, const vector<
 
 				if(distancia_nueva < distancia){
 					distancia = distancia_nueva;
-					distancia_corregida = distancia * distancia;
+					distancia_corregida = sqrt(distancia);
 					par = {cercanos_al_corte[i], cercanos_al_corte[j]};
 				}
 
